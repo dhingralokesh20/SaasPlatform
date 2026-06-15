193b5f3c636172572dc758fd4ab6e8e5efecb510
DhingraLokesh <dhingralokesh20@gmail.com>
2026-06-15 10:52:31 +0530

    added basic layout and routing for ui

--- apps/ui/src/app/app.routes.ts
+++ apps/ui/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.route').then(
+        (m) => m.AUTH_ROUTES,
+      ),
+  },
+  {
+    path: 'workspace',
+    loadChildren: () =>
+      import('./features/workspace/workspace.route').then(
+        (m) => m.WORKSPACE_ROUTES,
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
+  },
+];
